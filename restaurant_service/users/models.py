2196ee9fc065c1e05049695df0ebf5dfda1ad74b
sksmagr23 <sakshamag34@gmail.com>
2024-06-30 16:22:46 +0530

    created django project

---
+++ restaurant_service/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def _str_(self):
+        return self.name
---
+++ restaurant_service/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import MenuItem
+
+def home(request):
+    return render(request, 'users/home.html')
+
+@login_required
+def menu(request):
+    query = request.GET.get('q')
+    if query:
+        menu_items = MenuItem.objects.filter(Q(name_icontains=query) | Q(description_icontains=query))
+    else:
+        menu_items = MenuItem.objects.all()
+    return render(request, 'users/menu.html', {'menu_items': menu_items})
